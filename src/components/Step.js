c162e967955537a01a3bcf78fb088519c1ff3bcb
Tyler Stephen Maschino <tylermaschino@gmail.com>
2018-04-29 08:43:18 -0400

    Added Step component file

---
+++ src/components/Step.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "../Step.css";
+
+// Step component
+class Step extends Component {
+
+  toggleActive() {
+    // if (this.checked) {
+    console.log("Hello")
+    // }
+  }
+
+  render() {
+    return (
+      <div className="lineItem">
+        <span className="instruction-text">{this.props.instruction}</span>
+          <input
+          type="checkbox"
+          className="instruction-checkbox"
+          disabled={this.props.status == "incomplete" || this.props.status == "complete"}
+          onClick={this.toggleActive}
+          />
+        {this.props.image}
+      </div>
+    )
+  }
+}
+
+export default Step;
