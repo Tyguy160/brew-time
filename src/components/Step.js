52cd069dabd633ea81bf1b4b74ffaadcf24a41ef
Tyler Stephen Maschino <tylermaschino@gmail.com>
2018-05-04 06:24:40 -0400

    Added material design icons

--- src/components/Step.js
+++ src/components/Step.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import "../styles/Step.css";
+import 'pretty-checkbox'
+import 'material-design-icons'
 
 let Image = function generateImage(props) {
   let style = {
@@ -27,13 +29,20 @@ class Step extends Component {
     return (
       <div className="lineItem" key={this.props.key}>
         <span className="instruction-text">{this.props.instruction}</span>
-          <input
-          type="checkbox"
-          className="instruction-checkbox"
-          disabled={!this.props.isActive && !this.props.lastComplete}
-          onClick={() => this.props.clickHandler(this.props.id)}
-          />
+          <div className="pretty p-default p-round p-pulse">
+            <input
+            type="checkbox"
+            className="instruction-checkbox"
+            disabled={!this.props.isActive && !this.props.lastComplete}
+            onClick={() => this.props.clickHandler(this.props.id)}
+            />
+            <div className="state p-success">
+              <i className="icon material-icons"></i>
+              <label></label>
+            </div>
+          </div>
         {this.createImage(this.props.image,["instruction-image", !this.props.isActive ? "inactive" : ""])}
+
       </div>
     )
   }
